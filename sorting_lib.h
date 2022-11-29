5f25e3672a9759c1a763d9148ebf7354c7f5e5b1
BekirEmirhan <105277544+BekirEmirhan@users.noreply.github.com>
2022-11-29 14:53:48 +0300

    added bubble sort to library
    In this library we have 7 different sorting algorithm written in C

--- sorting_lib.h
+++ sorting_lib.h
@@ -196,3 +196,22 @@ void quickSort(double *arr, int low, int high) {
 } 
 
 
+
+/* Bubble Sort */
+
+
+void bubbleSort(double *arr,int size){
+	
+	for (int i=size-1;i>0;i--){
+		for(int j=0;j<i;j++){
+			if(arr[j]>arr[j+1]){
+				double temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
+		
+	}
+}
+
+
